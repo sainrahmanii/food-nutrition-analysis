db46b24c9c7b51d5d7a071681c5336a39a5e4f0f
sainrahmanii <husainrahmani127@gmail.com>
2025-06-08 03:05:12 +0700

    add system instruction for generative AI model in food nutrition analysis

--- lib/food_nutrition_composition.dart
+++ lib/food_nutrition_composition.dart
@@ -154,7 +154,28 @@ class _FoodNutritionCompositionState extends State<FoodNutritionComposition> {
             ElevatedButton(
               onPressed: () {
                 GenerativeModel model = GenerativeModel(
-                    model: 'gemini-1.5-flash-latest', apiKey: apiKey);
+                    model: 'gemini-1.5-flash-latest',
+                    apiKey: apiKey,
+                    systemInstruction: Content.system('''
+                        Kamu adalah seorang nutrisionis yang ahli dalam menganalisis komposisi nutrisi makanan.
+                        Tugas kamu adalah memberikan analisis yang akurat dan komprehensif tentang komposisi nutrisi, kandungan gizi, dan kehalalan dari gambar makanan yang diberikan.
+                        Kamu harus memberikan informasi yang jelas dan mudah dipahami, serta menyertakan rekomendasi jika diperlukan.
+                        Pastikan untuk selalu memberikan informasi yang sesuai dengan gambar yang diberikan.
+                        Jika gambar tidak jelas atau tidak dapat dianalisis, berikan penjelasan yang sesuai.
+                        berikan saya dalam bentuk json saja, tanpa penjelasan tambahan dengan format berikut:
+                        {
+                          "nama_makanan": "nama makanan",
+                          "komposisi_nutrisi": {
+                            "karbohidrat": "jumlah karbohidrat",
+                            "protein": "jumlah protein",
+                            "lemak": "jumlah lemak",
+                            "serat": "jumlah serat",
+                            "vitamin": "jenis vitamin"
+                          },
+                          "kehalalan": "status kehalalan (halal atau tidak halal)",
+                          infromasi tambahan: "informasi tambahan jika ada"
+                        }
+                      '''));
                 model.generateContent([]);
               },
               style: ElevatedButton.styleFrom(
